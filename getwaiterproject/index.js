6b7a94d8489d3b8cc73a045d665b48b3d4b97015
sara <s2114821148@gmail.com>
2019-10-29 13:26:53 +0200

    prigect

---
+++ getwaiterproject/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 4242;
+const registration = require("./registration.js");
+
+app.use(express.static('public'));
+app.use(cookieParser());
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile('./pages/home.html', {
+    root: __dirname
+}));
+app.get('/registration', (req, res) => res.sendFile('./public/pages/Registration.html', {
+    root: __dirname
+}));
+app.get('/contactus', (req, res) => res.sendFile('./public/pages/ContactUs.html', {
+    root: __dirname
+}));
+app.get('/home', (req, res) => res.sendFile('./public/pages/home.html', {
+    root: __dirname
+}));
+
+app.post('/registration/register', (req, res) => {
+    console.log('sghsth');
+    return registration.register(req, res);
+});
+app.post('/registration/login', (req, res) => {
+    return registration.login(req, res);
+});
+
+app.listen(port, () => console.log('Example app listening on port ' + port));
---
+++ getwaiterproject/registration.js
@@ -0,0 +1,12 @@
+module.exports = {
+    register,
+    login
+};
+
+function register(req, res) {
+    res.send('htgfmhgf');
+}
+
+function login(req, res) {
+    res.send("gfddd")
+}
